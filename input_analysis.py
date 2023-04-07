292bc398b55449510cc2e4fe1e315652f2e6e2ca
dimasvq <dimas.vq.2020@bristol.ac.uk>
2023-04-07 15:28:45 +0800

    adjusted number of bins of histograms

--- input_analysis.py
+++ input_analysis.py
@@ -2,18 +2,34 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def interarrival_time_histogram(df, bins=50):
+
+def interarrival_time_histogram(df):
     """Generates histogram for intearrival times"""
 
     sns.set(style='whitegrid')
-    sns.histplot(data=df, x='Interarrival time (sec)', bins=50, kde=True)
+    sns.histplot(data=df, x='Interarrival time (sec)', bins=40)
     plt.xlabel('Interarrival time (sec)')
     plt.ylabel('Frequency')
     plt.title('Histogram of interarrival time')
-    plt.xlim(0,9)
+    plt.xlim(0,8)
     plt.savefig('figures/interarrival_time_histogram.png')
     plt.show()
+
+
+def base_station_histogram(df):
+    """Generates histogram for base stations where calls are generated"""
+
+    sns.set(style='whitegrid')
+    sns.histplot(data=df, x='Base station ', bins=20)
+    plt.xlabel('Base station')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of base stations')
+    plt.xlim(0,20.5)
+    plt.savefig('figures/base_station_histogram.png')
+    plt.show()
+
 
 if __name__ == '__main__':
     df = pd.read_excel('simulation_data.xls')
     interarrival_time_histogram(df)
+    base_station_histogram(df)
