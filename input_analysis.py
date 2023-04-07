3682ea6585e3b0f6d7526da8c60157f63e7bb7b4
dimasvq <dimas.vq.2020@bristol.ac.uk>
2023-04-07 16:19:01 +0800

    added feature to calculate estimator, normalise histogram, and plot pdf

--- input_analysis.py
+++ input_analysis.py
@@ -1,22 +1,41 @@
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def interarrival_time_histogram(df):
-    """Generates histogram for intearrival times"""
+def interarrival_time_histogram(df, bins=40, pdf=False):
+    """Generates histogram for intearrival times, or normalised histogram
+    with estimated probability density function if pdf is set to True"""
 
     sns.set(style='whitegrid')
-    sns.histplot(data=df, x='Interarrival time (sec)', bins=40)
+    plt.figure()
     plt.xlabel('Interarrival time (sec)')
     plt.ylabel('Frequency')
     plt.title('Histogram of interarrival time')
-    plt.xlim(0,8)
-    plt.savefig('figures/interarrival_time_histogram.png')
+    xlim = 7.5
+    plt.xlim(0,xlim)
+
+    if pdf is False:
+        # Plot histogram.
+        sns.histplot(data=df, x='Interarrival time (sec)', bins=bins)
+        plt.savefig('figures/interarrival_time_histogram.png')
+
+    else:
+        # Plot normalised histogram.
+        sns.histplot(data=df, x='Interarrival time (sec)', bins=bins, stat='density')
+        # Calculate estimator for exponential distribution
+        x = np.linspace(0, xlim, 1000)
+        estimator = df['Interarrival time (sec)'].mean()
+        pdf = 1/estimator * np.exp(-x/estimator)
+        # Plot estimated pdf
+        plt.plot(x,pdf,'r')
+        plt.savefig('figures/interarrival_time_pdf.png')
+
     plt.show()
 
 
-def base_station_histogram(df):
+def base_station_histogram(df, pde=True):
     """Generates histogram for base stations where calls are generated"""
 
     sns.set(style='whitegrid')
@@ -29,7 +48,7 @@ def base_station_histogram(df):
     plt.show()
 
 
-def call_duration_histogram(df):
+def call_duration_histogram(df, pde=True):
     """Generates histogram for call duration"""
 
     sns.set(style='whitegrid')
@@ -57,7 +76,7 @@ def car_speed_histogram(df):
 
 if __name__ == '__main__':
     df = pd.read_excel('simulation_data.xls')
-    # interarrival_time_histogram(df)
+    interarrival_time_histogram(df,pdf=True)
     # base_station_histogram(df)
     # call_duration_histogram(df)
-    car_speed_histogram(df)
+    # car_speed_histogram(df)
