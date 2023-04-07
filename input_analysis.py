4579366a8d6d466bc2b25d89656735c75e70f8df
dimasvq <dimas.vq.2020@bristol.ac.uk>
2023-04-07 16:54:30 +0800

    added pdf option to all histogram functions

--- input_analysis.py
+++ input_analysis.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 
 def interarrival_time_histogram(df, bins=40, pdf=False):
@@ -65,29 +66,64 @@ def base_station_histogram(df, bins=20, pdf=False):
     plt.show()
 
 
-def call_duration_histogram(df, pde=True):
+def call_duration_histogram(df, bins=40, pdf=False):
     """Generates histogram for call duration"""
 
     sns.set(style='whitegrid')
-    sns.histplot(data=df, x='Call duration (sec)', bins=40)
+    plt.figure()
     plt.xlabel('Call duration')
     plt.ylabel('Frequency')
     plt.title('Histogram of call duration')
-    plt.xlim(0,600)
-    plt.savefig('figures/call_duration_histogram.png')
+    xlim = 550
+    plt.xlim(0, xlim)
+
+    if pdf is False:
+        # Plot histogram.
+        sns.histplot(data=df, x='Call duration (sec)', bins=bins)
+        plt.savefig('figures/call_duration_histogram.png')
+
+    else:
+        # Plot normalised histogram.
+        sns.histplot(data=df, x='Call duration (sec)', bins=bins, stat='density')
+        # Calculate maximum likelihood estimator for exponential distribution
+        x = np.linspace(0, xlim, 1000)
+        estimator = df['Call duration (sec)'].mean()
+        pdf = 1/estimator * np.exp(-x/estimator)
+        # Plot estimated pdf
+        plt.plot(x,pdf,'r--')
+        plt.savefig('figures/call_duration_pdf.png')
+    
     plt.show()
 
 
-def car_speed_histogram(df):
+def car_speed_histogram(df, bins=30, pdf=False):
     """Generates histogram for car speed"""
 
     sns.set(style='whitegrid')
-    sns.histplot(data=df, x='velocity (km/h)', bins=30)
+    plt.figure()
     plt.xlabel('Car speed')
     plt.ylabel('Frequency')
     plt.title('Histogram of car speed')
-    plt.xlim(90,152)
-    plt.savefig('figures/car_speed_histogram.png')
+    xlim_min = 90
+    xlim_max = 152
+    plt.xlim(xlim_min, xlim_max)
+
+    if pdf is False:
+        # Plot histogram
+        sns.histplot(data=df, x='velocity (km/h)', bins=bins)
+        plt.savefig('figures/car_speed_histogram.png')
+    
+    else:
+        sns.histplot(data=df, x='velocity (km/h)', bins=bins,stat='density')
+        # Calculate maximum likelihood estimators for normal distribution
+        x = np.linspace(xlim_min, xlim_max, 1000)
+        sample_mean = df['velocity (km/h)'].mean()
+        sample_stdev = df['velocity (km/h)'].std(ddof=1)
+        pdf = norm.pdf(x, loc=sample_mean, scale=sample_stdev)
+        # Plot estimated pdf
+        plt.plot(x,pdf,'r--')
+        plt.savefig('figures/car_speed_pdf.png')
+
     plt.show()
 
 
@@ -95,5 +131,5 @@ if __name__ == '__main__':
     df = pd.read_excel('simulation_data.xls')
     interarrival_time_histogram(df,pdf=True)
     base_station_histogram(df, pdf=True)
-    # call_duration_histogram(df)
-    # car_speed_histogram(df)
+    call_duration_histogram(df, pdf=True)
+    car_speed_histogram(df, pdf=True)
