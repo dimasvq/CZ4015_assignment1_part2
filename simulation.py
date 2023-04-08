072672476ed32498653dafcbeffe11714900287f
dimasvq <dimas.vq.2020@bristol.ac.uk>
2023-04-08 11:20:54 +0800

    initialise state variables and counters

--- simulation.py
+++ simulation.py
@@ -1,8 +1,24 @@
 import numpy as np
+from collections import deque
 
 class simulation():
     def __init__(self):
         """
         Simulation class
         """
-        
+
+        # Initialise state variables.
+        self.clock = 0
+        self.FEL = deque()
+        self.free_channels = [10]*20
+
+        # Initialise statistical counters.
+        blocked = 0
+        dropped = 0
+        total_calls = 0
+
+
+
+
+if __name__ == "__main__":
+    sim = simulation()
