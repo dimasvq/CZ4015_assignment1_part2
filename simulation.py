0cfa6d76465ca176dc59b3ef695b8a52e3496b4f
dimasvq <dimas.vq.2020@bristol.ac.uk>
2023-04-08 11:55:53 +0800

    added MLEs

--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
-import numpy as np
+import pandas as pd
+from numpy.random import exponential, randint
 from collections import deque
 
 class simulation():
@@ -12,10 +13,19 @@ class simulation():
         blocked = 0
         dropped = 0
         total_calls = 0
+
+        # Maximum likelihood estimators for probability distributions
+        self.df = pd.read_excel('simulation_data.xls')
+        self.interarrival_mean = self.df['Interarrival time (sec)'].mean()
+        self.station_min, self.station_max = 0, 20
+        self.duration_mean = self.df['Call duration (sec)'].mean()
+        self.speed_mean = self.df['velocity (km/h)'].mean()
+        self.speed_std = self.df['velocity (km/h)'].std(ddof=1)
     
 
     def call_initiation_event(self):
-        # self.clock += 
+        self.clock += exponential(self.interarrival_mean)
+        
 
 
 
