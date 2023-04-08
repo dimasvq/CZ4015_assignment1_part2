e1fb1745c1e900157739bb4c151305211a3e0e6e
dimasvq <dimas.vq.2020@bristol.ac.uk>
2023-04-08 13:50:03 +0800

    changed FEL from deque to heap

--- simulation.py
+++ simulation.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from numpy.random import exponential, randint, normal, uniform, choice
-from collections import deque
+from heapq import heappush, heappop
+
 
 class simulation():
     def __init__(self):
         # Initialise state variables.
         self.clock = 0
-        self.FEL = deque()
+        self.FEL = []
         self.free_channels = [10]*20
 
         # Initialise statistical counters.
@@ -22,18 +23,33 @@ class simulation():
         self.speed_mean = self.df['velocity (km/h)'].mean()
         self.speed_std = self.df['velocity (km/h)'].std(ddof=1)
     
+    def advance_time(self):
+        """Time advancement mechanism."""
+        self.FEL
+         
+    
 
-    def call_initiation_event(self):
+    def call_initiation_event(self, event):
+        """Initiation event handler."""
+        event_type, station, duration, speed, position, direction = event
+
+
+
+
+
+
         self.total_calls += 1
-        self.clock += exponential(self.interarrival_mean)
-        # Schedule
 
-        # Generate random variables for call initiation event
+        # Generate random variables for next call initiation event
         station = randint(self.station_min, self.station_max)
         duration = exponential(self.duration_mean)
         speed = normal(self.speed_mean, self.speed_std)
         position = uniform(low=0, high=2) # car position within cell
         direction = choice([-1,1])
+
+        # Schedule next call initiation event
+        self.clock += exponential(self.interarrival_mean)
+        self.FEL.append((self.clock, station, duration, speed, position, direction))
 
 
 
